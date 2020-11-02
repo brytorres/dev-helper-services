e169c451d5b271c9668e2e8501ed7c836db3deee
Bryan Torres <bryan@torvel.us>
2020-11-01 21:00:40 -0500

    add project files and entity

---
+++ src/app/projects/project.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, ManyToMany, JoinTable } from 'typeorm';
+import { DevelopmentLog } from '../development-log/development-log.entity';
+import { IssueLog } from '../issue-log/issue-log.entity';
+
+@Entity('projects')
+export class Project {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar', { length: 100, nullable: true })
+  name: string;
+
+  @Column('varchar', { length: 100, nullable: true })
+  title: string;
+
+  @Column('timestamp', { nullable: false })
+  dateStarted: string;
+
+  @Column('timestamp', { nullable: false })
+  dateCompleted: string;
+
+  @ManyToMany((type) => DevelopmentLog, (developmentLog) => developmentLog.projects)
+  @JoinTable()
+  developmentLogs: DevelopmentLog[];
+
+  @ManyToMany((type) => IssueLog, (issueLog) => issueLog.projects)
+  @JoinTable()
+  issueLogs: IssueLog[];
+}
