b171f8f8b63d595a74a3f3e699ccdf84e0fafd48
Bryan Torres <bryan@torvel.us>
2020-11-01 21:21:25 -0500

    cleanup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
   getHello(): string {
