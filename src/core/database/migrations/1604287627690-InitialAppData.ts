5e5091416d1b99301e6f57cffb7ea1b630e7ce94
Bryan Torres <bryan@torvel.us>
2020-11-01 23:59:35 -0500

    add test issue logs

--- src/core/database/migrations/1604287627690-InitialAppData.ts
+++ src/core/database/migrations/1604287627690-InitialAppData.ts
@@ -1,3 +1,4 @@
+import { IssueLog } from "src/app/issue-log/issue-log.entity";
 import { Project } from "src/app/projects/project.entity";
 import {getManager, MigrationInterface, QueryRunner} from "typeorm";
 
@@ -21,6 +22,33 @@ export class InitialAppData1604287627690 implements MigrationInterface {
     projectTrackerProject.name = 'projectTracker';
     projectTrackerProject.title = 'Project Tracker';
     await manager.insert(Project, projectTrackerProject);
+
+    const issue1 = new IssueLog();
+    issue1.dateTime = new Date('2020-10-30 22:32:37.000000').toISOString();
+    issue1.errorMessage = 'TypeError: type string is not the one it needs to be';
+    issue1.errorDescription = 'A type error happens when saving a document.';
+    issue1.errorStack = { foo: 'bar' };
+    issue1.keywords = ['get', 'test'];
+    issue1.solution = 'Lorem ipsum..dfads adsf al df';
+    await manager.insert(IssueLog, issue1);
+
+    const issue2 = new IssueLog();
+    issue2.dateTime = new Date('2020-10-31 22:32:37.000000').toISOString();
+    issue2.errorMessage = 'Invalid character';
+    issue2.errorDescription = 'An invalid character error shows up when running a SQL query.';
+    issue2.errorStack = { bar: 'baz' };
+    issue2.keywords = ['post', 'test'];
+    issue2.solution = 'Lorem ipsum..dfads adsf al df';
+    await manager.insert(IssueLog, issue2);
+
+    const issue3 = new IssueLog();
+    issue3.dateTime = new Date('2020-11-01 22:32:37.000000').toISOString();
+    issue3.errorMessage = 'Test';
+    issue3.errorDescription = 'Short test one.';
+    issue3.errorStack = { foo: 'bar' };
+    issue3.keywords = ['food'];
+    issue3.solution = 'Lorem ipsum..dfads adsf al df';
+    await manager.insert(IssueLog, issue3);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
